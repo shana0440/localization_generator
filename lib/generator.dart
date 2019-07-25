295fcc71c466689a8c52295fa8e99c768eaa0b3d
kwguo <shana0440@gmail.com>
2019-07-26 00:59:15 +0800

    Add generator

---
+++ lib/generator.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+
+import './template.dart';
+
+class Generator {
+  Directory output;
+
+  Generator(String output) {
+    this.output = Directory(output);
+  }
+
+  Map<String, Map> localizations = Map();
+
+  void load(String locale, Map json) {
+    localizations[locale] = json;
+  }
+
+  void generate() {
+    if (!this.output.existsSync()) {
+      this.output.createSync();
+    }
+    var template = getTemplate();
+    template = replaceSupportedLanguages(template);
+    template = replaceLocalizedGenerateFactory(template);
+    template = replaceFirstLocalized(template);
+    template = replaceOtherLocalizedClasses(template);
+    File(output.path + '/localization.dart').writeAsStringSync(template);
+  }
+
+  String getTemplate() {
+    return template;
+  }
+
+  String replaceSupportedLanguages(String template) {
+    var supportedLanguages = this.localizations.keys
+      .map((locale) => indent(6, 'Locale("$locale", ""),'));
+    return template.replaceFirst(
+      '{SupportedLanguages}', 
+      supportedLanguages.join('\n'),
+    );
+  }
+
+  String replaceLocalizedGenerateFactory(String template) {
+    var supportedLanguages = this.localizations.keys
+      .map((locale) => 
+        indent(8, 'case "$locale":\n') + 
+        indent(10, "return SynchronousFuture<Localized>(const \$$locale());")
+      );
+    return template.replaceFirst(
+      '{LocalizedGenerateFactory}', 
+      supportedLanguages.join('\n'),
+    );
+  }
+
+  String replaceFirstLocalized(String template) {
+    var first = this.localizations.keys.first;
+    var localized = this.localizations[first];
+    var localizedStrings = generateLocalizedStrings(localized);
+    return template.replaceFirst('{FisrtLocalizedString}', localizedStrings)
+      .replaceFirst('{FirstLocalizedClass}', '''
+class \$$first extends Localized {
+  const \$$first();
+}''');
+  }
+
+  String replaceOtherLocalizedClasses(String template) {
+    var classes = this.localizations.keys.skip(1).map((key) {
+      var localized = this.localizations[key];
+      var localizedStrings = generateLocalizedStrings(localized);
+      return '''
+class \$$key extends Localized {
+  const \$$key();
+
+  @override
+  TextDirection get textDirection => TextDirection.ltr;
+
+  @override
+$localizedStrings
+}
+''';
+    });
+    return template.replaceFirst('{OtherLocalizedClasses}', classes.join('\n'));
+  }
+
+  String generateLocalizedStrings(Map json) {
+    return json
+      .map((key, value) => MapEntry(key, indent(2, 'String get $key => "$value";')))
+      .values.join('\n');
+  }
+
+  String indent(int number, String str) {
+    return ' ' * number + str;
+  }
+}
---
+++ test/generator_test.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:localization_generator/generator.dart';
+
+void main() {
+  test('Test generator can generate the Localization file', () {
+    String testingOutputFolder = Directory.current.path + '/generated';
+    final generator = Generator(testingOutputFolder);
+    var enJSON = jsonDecode(
+      '''
+      {
+        "PackageTitle": "Localization Generator",
+        "PackageDescription": "Use to generate localization code from json file"
+      }
+      '''
+    );
+    var zhJSON = jsonDecode(
+      '''
+      {
+          "PackageTitle": "多國語系產生器",
+          "PackageDescription": "從json檔案產出多國語系的程式"
+      }
+      '''
+    );
+    generator.load('en', enJSON);
+    generator.load('zh', zhJSON);
+    generator.generate();
+    expect(
+      File('./generated/localization.dart').readAsStringSync(), 
+      File('./generated/localization_snapshot.dart').readAsStringSync()
+    );
+  });
+}

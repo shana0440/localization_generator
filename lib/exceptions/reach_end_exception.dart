ab1fb85824525c316dc9232b8ef327bb375df2a8
kwguo <shana0440@gmail.com>
2021-04-14 18:00:40 +0800

    Refactor icu parser

---
+++ lib/exceptions/reach_end_exception.dart
@@ -0,0 +1,1 @@
+class ReachEndException extends Error {}
--- lib/parser/icu_parser.dart
+++ lib/parser/icu_parser.dart
@@ -3,6 +3,7 @@ import 'package:localization_generator/exceptions/invalid_choice_exception.dart'
 import 'package:localization_generator/exceptions/invalid_option_exception.dart';
 import 'package:localization_generator/exceptions/missing_close_curly_exception.dart';
 import 'package:localization_generator/exceptions/missing_open_curly_exception.dart';
+import 'package:localization_generator/exceptions/reach_end_exception.dart';
 import 'package:localization_generator/parser/parser.dart';
 import 'package:localization_generator/parser/token.dart';
 
@@ -14,9 +15,6 @@ class ICUParser implements Parser {
 
   final _openCurly = Str('{');
   final _closeCurly = Str('}');
-  final _letter = Letter();
-  final _digits = Digits();
-  final _underline = Str('_');
   final _singleQuote = Str("'");
   final _doubleSingleQuote = Str("''");
   final _comma = Str(',');
@@ -24,6 +22,7 @@ class ICUParser implements Parser {
   final _gender = Str('gender');
   final _plural = Str('plural');
   final _hashtag = Str('#');
+  final _validArgumentRegex = RegExp(r"^[a-zA-Z]\w*");
 
   @override
   List<Node> parse(String value) {
@@ -32,181 +31,149 @@ class ICUParser implements Parser {
     return _doParse(State.Message);
   }
 
-  List<Node> _doParse(State state) {
+  List<Node> _doParse(State state, [TokenType? endAt]) {
     final List<Node> nodes = [];
-    String message = "";
     bool isEscaping = false;
 
-    void addMessageNode(String message) {
+    String message = "";
+    void addMessageNode() {
       if (message != "") {
         nodes.add(MessageNode(message));
       }
+      message = "";
     }
 
-    // stop when reach end, or reach close curly if not escaping
-    while (!_reachEnd && (isEscaping || !_predict(_closeCurly))) {
-      if (_predict(_singleQuote) && !_predict(_doubleSingleQuote)) {
-        _take(); // take away single quote
+    while (!_reachEnd && (endAt == null || !_predict(endAt))) {
+      if (_predict(_singleQuote) &&
+          !_predict(_doubleSingleQuote, falseWhenReachEnd: true)) {
         isEscaping = !isEscaping;
       }
 
-      if (_predict(_openCurly) && !isEscaping) {
-        addMessageNode(message);
-        message = "";
+      if (_predict(_openCurly, takeAwayWhenPredicted: true) && !isEscaping) {
+        addMessageNode();
 
-        _take();
-        nodes.add(_or([
-          _parseArgument,
-          _parseSelect,
-          _parseGender,
-          _parsePlural,
-        ]));
-      } else if (_predict(_hashtag) && !isEscaping && state == State.Plural) {
-        addMessageNode(message);
-        message = "";
+        String argument = "";
+        try {
+          argument = _takeUntil([_comma, _closeCurly]);
+          if (!_validArgumentRegex.hasMatch(argument)) {
+            throw InvalidArgumentException();
+          }
+        } on ReachEndException {
+          throw MissingCloseCurlyException();
+        }
 
-        _take();
+        if (_predict(_closeCurly, takeAwayWhenPredicted: true)) {
+          nodes.add(ArgumentNode(argument));
+          continue;
+        }
+
+        if (_predict(_comma, takeAwayWhenPredicted: true)) {
+          try {
+            String choice = _takeUntil([_comma]);
+            _take(); // take away comma
+            if (_select.isOk(choice)) {
+              final options = _parseOptions(State.Select);
+              nodes.add(SelectNode(argument, options));
+            } else if (_plural.isOk(choice)) {
+              final options = _parseOptions(State.Plural, [
+                '=0',
+                '=1',
+                '=2',
+                'zero',
+                'one',
+                'two',
+                'few',
+                'many',
+                'other'
+              ]);
+              nodes.add(PluralNode(argument, options));
+            } else if (_gender.isOk(choice)) {
+              final options =
+                  _parseOptions(State.Gender, ['female', 'male', 'other']);
+              nodes.add(GenderNode(argument, options));
+            } else {
+              throw InvalidChoiceException();
+            }
+          } on ReachEndException {
+            throw InvalidChoiceException();
+          }
+        }
+        if (!_predict(_closeCurly, takeAwayWhenPredicted: true)) {
+          throw MissingCloseCurlyException();
+        }
+      } else if (state == State.Plural &&
+          _predict(_hashtag, takeAwayWhenPredicted: true) &&
+          !isEscaping) {
+        addMessageNode();
         nodes.add(HashTagNode());
       } else {
-        if (_predict(_doubleSingleQuote)) {
-          _take(); // take away single quote
+        _predict(_singleQuote,
+            falseWhenReachEnd: true, takeAwayWhenPredicted: true);
+        if (_predict(_closeCurly, falseWhenReachEnd: true) && !isEscaping) {
+          throw MissingOpenCurlyException();
         }
-        if (!_reachEnd) {
+        // FIXME: keep exit loop condition in one place
+        if (!_reachEnd && (endAt == null || !_predict(endAt))) {
           message += _take();
         }
       }
     }
-
-    addMessageNode(message);
-
-    if (_predict(_closeCurly)) {
-      _take();
-    }
+    addMessageNode();
 
     return nodes;
-  }
-
-  Node _parseArgument() {
-    String argument = "";
-    if (!_predict(_letter)) {
-      throw InvalidArgumentException();
-    }
-
-    while (!_reachEnd && !_predict(_closeCurly)) {
-      final isValidArgument =
-          _predict(_letter) || _predict(_digits) || _predict(_underline);
-      if (isValidArgument) {
-        final took = _take();
-        // get argument name
-        argument += took;
-      } else {
-        throw InvalidArgumentException();
-      }
-    }
-
-    if (_predict(_closeCurly)) {
-      _take();
-      return ArgumentNode(argument);
-    }
-
-    throw MissingCloseCurlyException();
-  }
-
-  Node _parseSelect() {
-    final argument = _takeUntil(_comma);
-    if (!_select.isOk(_takeUntil(_comma))) {
-      throw InvalidChoiceException();
-    }
-    final options = _parseOptions(State.Select);
-    return SelectNode(argument, options);
-  }
-
-  Node _parseGender() {
-    final argument = _takeUntil(_comma);
-    if (!_gender.isOk(_takeUntil(_comma))) {
-      throw InvalidChoiceException();
-    }
-    final options = _parseOptions(State.Gender, ['female', 'male', 'other']);
-    return GenderNode(argument, options);
-  }
-
-  Node _parsePlural() {
-    final argument = _takeUntil(_comma);
-    if (!_plural.isOk(_takeUntil(_comma))) {
-      throw InvalidChoiceException();
-    }
-    final options = _parseOptions(
-      State.Plural,
-      ['=0', '=1', '=2', 'zero', 'one', 'two', 'few', 'many', 'other'],
-    );
-    return PluralNode(argument, options);
   }
 
   Map<String, List<Node>> _parseOptions(State state,
       [List<String>? allowValue]) {
     final Map<String, List<Node>> options = {};
-    while (!_reachEnd && !_predict(_closeCurly)) {
-      String value = "";
-      while (!_reachEnd && !_predict(_openCurly)) {
-        final took = _take();
-        value += took;
-      }
+    while (!_predict(_closeCurly)) {
+      String value = _takeUntil([_openCurly]);
+      _take(); // take away open curly
       if (allowValue != null && !allowValue.contains(value.trim())) {
         throw InvalidOptionException(allowValue, value);
       }
-      if (_predict(_openCurly)) {
-        _take();
-        final metadata = _doParse(state);
-        options[value.trim()] = metadata;
-        continue;
-      }
-
       if (_reachEnd) {
         throw MissingOpenCurlyException();
       }
-    }
 
-    if (_predict(_closeCurly)) {
-      _take();
-      return options;
+      final metadata = _doParse(state, _closeCurly);
+      _take(); // take away close curly
+      options[value.trim()] = metadata;
+      continue;
     }
-    throw MissingCloseCurlyException();
+    return options;
   }
 
-  Node _or(List<Node Function()> parsers) {
-    final errors = [];
-    final offset = _offset;
-    for (final parser in parsers) {
-      try {
-        return parser.call();
-      } catch (e) {
-        errors.add(e);
-        _offset = offset;
+  bool _predict(TokenType token,
+      {bool falseWhenReachEnd = false, takeAwayWhenPredicted: false}) {
+    if ((_offset + token.length) > _value.length) {
+      if (falseWhenReachEnd) {
+        return false;
       }
-    }
-    throw errors.last;
-  }
-
-  bool _predict(TokenType token) {
-    if (_offset + token.length > _value.length) {
-      return false;
+      throw ReachEndException();
     }
     final tk = _value.substring(_offset, _offset + token.length);
-    return token.isOk(tk);
+    final ok = token.isOk(tk);
+    if (takeAwayWhenPredicted && ok) {
+      _take();
+    }
+    return ok;
   }
 
   String _take() {
+    if (_reachEnd) {
+      throw ReachEndException();
+    }
     final value = _value[_offset];
     _offset += 1;
     return value;
   }
 
-  String _takeUntil(TokenType token) {
+  String _takeUntil(List<TokenType> tokens) {
     String took = "";
-    while (!_predict(token)) {
+    while (!tokens.any((it) => _predict(it))) {
       took += _take();
     }
-    _take(); // take token
     return took;
   }
 
